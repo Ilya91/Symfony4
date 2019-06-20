f05c5f3efbfbc6a6a82ae3c364ced688fba5340a
Mironchikov Ilya <i.mironchikov@gmail.com>
2019-06-20 17:58:32 +0300

    fixtures

---
+++ manager/src/DataFixtures/PostFixtures.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Post;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
+
+class PostFixtures extends Fixture
+{
+    /**
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        $faker = Factory::create();
+        for ($i = 0; $i < 30; $i++) {
+            $post = new Post();
+            $post->setTitle($faker->words($nb = 3));
+            $post->setContent($faker->text);
+            $post->setUserId($faker->numberBetween(1, 9));
+            $post->setCreatedAt($faker->dateTime($max = 'now', $timezone = null));
+            $post->setUpdatedAt($faker->dateTime($max = 'now', $timezone = null));
+            $manager->persist($post);
+        }
+
+        $manager->flush();
+    }
+}
