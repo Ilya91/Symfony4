40d377b0a717147368e9282ded9b7377e7d21aa5
Mironchikov Ilya <i.mironchikov@gmail.com>
2019-07-11 16:09:40 +0300

    added role for registered user

--- manager/src/Controller/RegistrationController.php
+++ manager/src/Controller/RegistrationController.php
@@ -30,7 +30,7 @@ class RegistrationController extends AbstractController
 
         if ($form->isSubmitted() && $form->isValid()) {
             // encode the plain password
-            $user->setRoles(['ROLE_USER']);
+            $user->setRoles(['ROLE_USER', 'ROLE_ADMIN']);
             $user->setPassword(
                 $passwordEncoder->encodePassword(
                     $user,
